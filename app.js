a701e29b6b3734b6f2711bac1a0c773589d8221e
elena-in-code <30567608+elena-in-code@users.noreply.github.com>
2018-02-18 13:43:15 +0100

    getJson

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
-const btn1 = document.getElementById("button1");
+const btn1 = document.getElementById("button1"),
+      btn2 = document.getElementById("button2"),
+      output = document.getElementById("output");
 
 btn1.addEventListener("click", getText);
+btn2.addEventListener("click", getJson);
 
-function getText () {
-    fetch("test1.txt")
+function getText() {
+    fetch("test.txt")
     .then(function(res){
         return res.text();
     })
     .then(function(data){
-        console.log(data);
+        output.innerHTML = data;
+    })
+    .catch(function(err) {
+        console.log(err);
+    });
+}
+
+function getJson() {
+    fetch("posts.json")
+    .then(function(res){
+        return res.json();
+    })
+    .then(function(data){
+        //console.log(data);
+        let answer ="";
+        data.forEach(function(post){
+            answer += `<li>${post.title}: ${post.body}</li>`;
+        });
+        output.innerHTML = answer;
     })
     .catch(function(err) {
         console.log(err);
