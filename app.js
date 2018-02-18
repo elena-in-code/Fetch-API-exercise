087ca0e1f6691e5f06cc804bf7cdf2f92efb4880
elena-in-code <30567608+elena-in-code@users.noreply.github.com>
2018-02-18 14:02:23 +0100

    getExternal API
    use github api

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const btn1 = document.getElementById("button1"),
       btn2 = document.getElementById("button2"),
+      btn3 = document.getElementById("button3"),
       output = document.getElementById("output");
 
 btn1.addEventListener("click", getText);
 btn2.addEventListener("click", getJson);
+btn3.addEventListener("click", getExternal);
 
 function getText() {
     fetch("test.txt")
@@ -28,6 +30,24 @@ function getJson() {
         let answer ="";
         data.forEach(function(post){
             answer += `<li>${post.title}: ${post.body}</li>`;
+        });
+        output.innerHTML = answer;
+    })
+    .catch(function(err) {
+        console.log(err);
+    });
+}
+
+function getExternal() {
+    fetch("https://api.github.com/users")
+    .then(function(res){
+        return res.json();
+    })
+    .then(function(data){
+        //console.log(data);
+        let answer ="";
+        data.forEach(function(user){
+            answer += `<li><img src="${user.avatar_url}" alt="user avatar image" height="50" width="50"> ${user.login}</li>`;
         });
         output.innerHTML = answer;
     })
