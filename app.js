7524411ad899070c7486d836ad41b94371f48818
elena-in-code <30567608+elena-in-code@users.noreply.github.com>
2018-02-18 13:24:52 +0100

    getText

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ const btn1 = document.getElementById("button1");
 btn1.addEventListener("click", getText);
 
 function getText () {
-    fetch("test.txt")
+    fetch("test1.txt")
     .then(function(res){
-        console.log(res);
+        return res.text();
+    })
+    .then(function(data){
+        console.log(data);
+    })
+    .catch(function(err) {
+        console.log(err);
     });
 }
